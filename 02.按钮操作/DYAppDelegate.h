11962c85dddc7ddf68426b745c2b8c7c027c6f44
Adiya <libera1986@hotmail.com>
2015-03-28 13:55:34 +0800

    Initial Commit

---
+++ 02.按钮操作/DYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DYAppDelegate.h
+//  02.按钮操作
+//
+//  Created by YOU on 15/3/28.
+//  Copyright (c) 2015年 YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 02.按钮操作/DYViewController.m
@@ -0,0 +1,29 @@
+//
+//  DYViewController.m
+//  02.按钮操作
+//
+//  Created by YOU on 15/3/28.
+//  Copyright (c) 2015年 YOU. All rights reserved.
+//
+
+#import "DYViewController.h"
+
+@interface DYViewController ()
+
+@end
+
+@implementation DYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

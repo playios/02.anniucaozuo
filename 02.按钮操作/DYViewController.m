1b0aa26f7d5f0f079d75fbe9021d9c1bcd0371c8
Adiya <libera1986@hotmail.com>
2015-03-28 14:17:29 +0800

    完成位置移动和大小改变

--- 02.按钮操作/DYViewController.m
+++ 02.按钮操作/DYViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "DYViewController.h"
+typedef enum {
+kMoveDirTop=10,
+    kMoveDirLeft,
+    kMoveDirRight,
+    kMoveDirBottom
+} kMoveDir;
 
+#define kMvoeDelate 20
 @interface DYViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *iconBtn;
 
 @end
 
@@ -24,6 +32,40 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)move:(UIButton *)button{
+    CGRect frame=self.iconBtn.frame;
+    switch (button.tag) {
+        case kMoveDirTop:
+            frame.origin.y-=kMvoeDelate;
+            break;
+        case kMoveDirLeft:
+            frame.origin.x-=kMvoeDelate;
+            break;
+        case kMoveDirRight:
+            frame.origin.x+=kMvoeDelate;
+            break;
+        case kMoveDirBottom:
+            frame.origin.y+=kMvoeDelate;
+            break;
+            
+    }
+    self.iconBtn.frame=frame;
+}
+
+
+-(IBAction)zoom :(UIButton *)button{
+    CGRect frame=self.iconBtn.bounds;
+
+    if (button.tag) {
+        frame.size.height+=50;
+        frame.size.width+=50;
+    }else{
+        frame.size.height-=50;
+        frame.size.width-=50;
+    }
+       self.iconBtn.bounds=frame;
 }
 
 @end
